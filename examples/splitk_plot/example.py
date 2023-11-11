Bugfix in I opt for numerical stability

examples/splitk_plot/example.py
@@ -35,9 +35,9 @@ model = partial_rsm_names({key: value[1] for key, value in effects.items()})
 metric = Aopt()
 
 # Define prior
-prior = (pd.read_csv('example_design.csv'), np.array([4, 6]))
-plot_sizes = np.array([5, 6])
-# prior = None
+# prior = (pd.read_csv('example_design.csv'), np.array([4, 6]))
+# plot_sizes = np.array([5, 6])
+prior = None
 
 # Define multiple ratios
 ratios = np.stack((np.ones(1) * 10, np.ones(1) * 0.1))
@@ -45,7 +45,7 @@ ratios = np.stack((np.ones(1) * 10, np.ones(1) * 0.1))
 #########################################################################
 
 # Parameter initialization
-n_tries = 100
+n_tries = 1000
 
 # Create the set of operators
 fn = default_fn(metric)
@@ -54,7 +54,7 @@ fn = default_fn(metric)
 start_time = time.time()
 Y, state = create_splitk_plot_design(
     fn, effect_types, effect_levels, plot_sizes, ratios=ratios,
-    model=model, prior=prior, n_tries=n_tries, validate=True
+    model=model, prior=prior, n_tries=n_tries, validate=False
 )
 end_time = time.time()
 

src/pyoptex/doe/splitk_plot/metric.py
@@ -8,7 +8,8 @@ from .init import init_random
 from .formulas import det_update_UD, inv_update_UD, inv_update_UD_no_P
 from ..utils.comp import outer_integral
 
-# TODO: covariances
+# TODO: covariates
+# TODO: blocking
 # TODO: plot_sizes = lowest first: note in documentation!
 
 class Dopt:
@@ -68,7 +69,11 @@ class Aopt:
 
     def update(self, Y, X, update):
         # Compute update to Minv
-        self.Mup = inv_update_UD_no_P(update.U, update.D, self.Minv)
+        try:
+            self.Mup = inv_update_UD_no_P(update.U, update.D, self.Minv)
+        except np.linalg.LinAlgError as e:
+            # Infeasible design
+            return -np.inf
 
         # Compute update to metric (double negation with update)
         metric_update = np.mean(np.trace(self.Mup, axis1=-2, axis2=-1))
@@ -132,10 +137,18 @@ class Iopt:
 
     def update(self, Y, X, update):
         # Compute update to Minv
-        self.Mup = inv_update_UD_no_P(update.U, update.D, self.Minv)
+        try:
+            self.Mup = inv_update_UD_no_P(update.U, update.D, self.Minv)
+        except np.linalg.LinAlgError as e:
+            # Infeasible design
+            return -np.inf
 
         # Compute update to metric (double negation with update)
         metric_update = np.mean(np.sum(self.Mup * self.moments.T, axis=(1, 2))) / self.intdx
+
+        # Numerical instability (negative variance)
+        if metric_update > -update.old_metric:
+            metric_update = -np.inf
 
         return metric_update
 