bugfix in formula + documentation

src/pyoptex/doe/cost_optimal/formulas.py
@@ -203,8 +203,8 @@ def add_update_vinv(Vinv, Zs, a, pos, ratios):
     # Compute inner products Zi and ai
     Zs_valid = np.array([i for i in range(len(Zs)) if Zs[i] is not None])
     B = np.zeros((Vinv.shape[0], Vinv.shape[-1], len(Zs_valid)))
-    for i in Zs_valid:
-        B[:, Zs[i] == a[i], :] = ratios[:, np.newaxis, Zs_valid]
+    for j, i in enumerate(Zs_valid):
+        B[:, Zs[i] == a[i], j] = ratios[:, np.newaxis, i]
     B = np.sum(B, axis=-1)
 
     # Initialize matrix

src/pyoptex/doe/cost_optimal/metric.py
@@ -39,6 +39,8 @@ class Aopt:
     Attributes:
     cov : func
         A function computing the covariate parameters and potential extra random effects.
+    W : np.array(1d)
+        A weights matrix for the trace of the inverse of the information matrix.
     """
     def __init__(self, cov=None, W=None):
         self.cov = cov or no_cov
@@ -53,7 +55,7 @@ class Aopt:
         M = X.T @ Vinv @ X
 
         # Check if invertible (more stable than relying on inverse)
-        if np.linalg.matrix_rank(X) == X.shape[1]:
+        if np.linalg.matrix_rank(M[0]) == M.shape[1]:
             # Extrace variances
             diag = np.diag(np.linalg.inv(M), axis1=-2, axis2=-1)
 
@@ -92,6 +94,7 @@ class Iopt:
     def __init__(self, n=10000, cov=None):
         self.cov = cov or no_cov
         self.moments = None
+        self.samples = None
         self.intdx = None
         self.n = n
 
@@ -114,7 +117,7 @@ class Iopt:
         M = X.T @ Vinv @ X
 
         # Check if invertible (more stable than relying on inverse)
-        if np.linalg.matrix_rank(X) == X.shape[1]:
+        if np.linalg.matrix_rank(M[0]) == M.shape[1]:
             # Compute average trace (normalized)
             trace = np.mean(np.trace(np.linalg.solve(
                 M, 
@@ -133,8 +136,12 @@ class Aliasing:
     Attributes:
     cov : func
         A function computing the covariate parameters and potential extra random effects.
-    W : np.array
-        A potential weighting matrix for the elements in M.
+    W : np.array(2d)
+        A potential weighting matrix for the elements in aliasing matrix A.
+    effects : np.array(1d)
+        An array of indices indicating the effects (=rows in aliasing matrix).
+    alias : np.array(1d)
+        An array of indices indicating the terms to which we alias (=cols in aliasing matrix).
     """
     def __init__(self, effects, alias, cov=None, W=None):
         self.cov = cov or no_cov

src/pyoptex/doe/splitk_plot/init.py
@@ -10,7 +10,7 @@ from ..._profile import profile
 @numba.njit
 def __init_unconstrained(effect_types, effect_levels, grps, thetas, coords, Y, complete=False):
     """
-    This function is created to avoid possible recursion. Numba has issues with it.
+    This function generated a random design without considering any design constraints.
 
     .. note::
         See :py:func:`initialize_single` for more information
@@ -68,6 +68,11 @@ def __init_unconstrained(effect_types, effect_levels, grps, thetas, coords, Y, c
 
 @numba.njit
 def __correct_constraints(effect_types, effect_levels, grps, thetas, coords, plot_sizes, constraints, Y, complete=False):
+    """
+    Corrects an unconstrained design randomly to be within the provided constraints
+    It alters the factors starting from the most hard to change factors to the most easy to change
+    until all constraints are met.
+    """
     # Check which runs are invalid
     invalid_run = constraints(Y)
 
@@ -127,10 +132,21 @@ def initialize_feasible(params, complete=False, max_tries=10):
 
     Parameters
     ----------
+    params : `pyoptex.doe.splitk_plot.utils.Parameters`
+        The parameters of the design generation.
+    complete : bool
+        Whether to initialize based on the discrete coordinates provided
+        in the `params`, or to initiliaze continuous variables based on the entire
+        range between -1 and 1.
+    max_tries : int
+        The maximum number of tries to generate a feasible design.
 
     Returns
     -------
-
+    Y : np.array(2d)
+        The generated design.
+    enc : tuple(np.array(2d), np.array(2d))
+        The categorical factor encoded Y and X respectively.
     """
     # Compute design sizes
     n = np.prod(params.plot_sizes)

src/pyoptex/doe/splitk_plot/metric.py
@@ -12,9 +12,15 @@ class Dopt:
     """
     The D-optimality criterion.
     Computes the geometric mean in case multiple Vinv are provided.
+
+    Attributes
+    ----------
+    Minv : np.array(3d)
+        The inverse of the information matrix. This is used as a cache.
+    P : np.array(2d)
+        The P-matrix in the update formula. This is used as a cache.
     """
     def __init__(self):
-        self.c = None
         self.Minv = None
         self.P = None
 
@@ -50,6 +56,13 @@ class Aopt:
     """
     The A-optimality criterion.
     Computes the average trace if multiple Vinv are provided.
+
+    Attributes
+    ----------
+    Minv : np.array(3d)
+        The inverse of the information matrix. This is used as a cache.
+    Mup : np.array(3d)
+        The update to the inverse of the information matrix. This is used as a cache.
     """
     def __init__(self):
         self.Minv = None
@@ -102,7 +115,8 @@ class Iopt:
     The I-optimality criterion.
     Computes the average (average) prediction variance if multiple Vinv are provided.
 
-    Attributes:
+    Attributes
+    ----------
     moments : np.array(2d)
         The moments matrix.
     samples : np.array(2d)
@@ -111,6 +125,10 @@ class Iopt:
         The integral over the input space for normalization.
     n : int
         The number of samples.
+    Minv : np.array(3d)
+        The inverse of the information matrix. Used as a cache.
+    Mup : np.array(3d)
+        The update to the inverse of the information matrix. Used as a cache.
     """
     def __init__(self, n=10000):
         self.moments = None