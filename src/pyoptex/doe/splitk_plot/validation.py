171d01d28d801393d876f010cd5aac154dde03dc
Mathias Born <mathias.born@cnhind.com>
2024-11-22 10:18:26 +0100

    Small bugfix

--- src/pyoptex/doe/splitk_plot/validation.py
+++ src/pyoptex/doe/splitk_plot/validation.py
@@ -27,6 +27,7 @@ def validate_state(state, params, eps=1e-6):
     assert np.all(state.X == params.Y2X(state.Y)), 'X does not match Y2X(Y)'
 
     # Validate metric
+    metric = params.fn.metric.call(state.Y, state.X, params)
     if (metric == 0 and state.metric == 0) \
         or (np.isnan(metric) and np.isnan(state.metric))\
         or (np.isinf(metric) and np.isinf(state.metric)):
