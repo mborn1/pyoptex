48318d18a583e3dac733987db6a109a382a90411
Mathias Born <mathiasborn2@gmail.com>
2025-04-27 12:35:20 +0200

    Updated optimization

--- src/pyoptex/doe/fixed_structure/_optimize_cy.pyx
+++ src/pyoptex/doe/fixed_structure/_optimize_cy.pyx
@@ -2,31 +2,46 @@
 # To compile this file (from the project root directory): python setup.py build_ext --inplace
 
 import numpy as np
-cimport numpy as np
+cimport numpy as cnp
+from libc.math cimport isinf, abs
 
 # Import State for type checking, though we'll handle its components
 from .utils import State
 
 # Ensure NumPy C API is initialized (important for Cython extensions using NumPy)
-np.import_array()
+cnp.import_array()
+
+cdef bint cython_equal(const double[::1] a, const double[::1] b, Py_ssize_t n) noexcept:
+    cdef Py_ssize_t idx
+    for idx in range(n):
+        if a[idx] != b[idx]:
+            return False
+    return True
+    
+cdef bint cython_any(unsigned char[::1] arr, Py_ssize_t n) noexcept:
+    cdef Py_ssize_t i
+    for i in range(n):
+        if arr[i]:
+            return True
+    return False
 
 # Define the Cython implementation function
 cpdef _optimize_cython_impl(object params, int max_it, bint validate, double eps):
     """Cython implementation of the core coordinate-exchange logic."""
     # Type declarations for parameters accessed frequently
-    cdef np.ndarray[np.intp_t, ndim=1, mode='c'] effect_types = params.effect_types
-    cdef np.ndarray[np.intp_t, ndim=1, mode='c'] effect_levels = params.effect_levels
+    cdef long[::1] effect_types = params.effect_types
+    cdef long[::1] effect_levels = params.effect_levels
     cdef object grps = params.grps
-    cdef object coords = params.coords
-    cdef np.ndarray[np.intp_t, ndim=1, mode='c'] colstart = params.colstart
-    cdef np.ndarray[np.intp_t, ndim=2, mode='c'] Zs = params.Zs
+    cdef object coords = params.coords # TODO: rework initialization for this
+    cdef long[::1] colstart = params.colstart
+    cdef long[:,::1] Zs = params.Zs
     cdef object fn = params.fn # Keep fn as a Python object for callbacks
 
     ###############################################
 
     # Initialize a design (Call Python function)
-    cdef np.ndarray[np.double_t, ndim=2, mode='c'] Y
-    cdef np.ndarray[np.double_t, ndim=2, mode='c'] X
+    cdef cnp.ndarray[cnp.double_t, ndim=2, mode='c'] Y
+    cdef cnp.ndarray[cnp.double_t, ndim=2, mode='c'] X
     _, (Y, X) = fn.init(params)
 
     # Use memoryviews for efficient access in loops
@@ -42,19 +57,23 @@ cpdef _optimize_cython_impl(object params, int max_it, bint validate, double eps
     ###############################################
 
     # Loop variables
-    cdef int it, i, level, grp_val, nruns
-    cdef Py_ssize_t j, k, run_idx, col_idx, slice_len
+    cdef int it, h, i, j, level, grp_val, nruns
+    cdef Py_ssize_t k, run_idx, slice_len
     cdef bint updated = False
     cdef double new_metric, up
 
     # Temporary arrays/views needed inside loops
-    cdef long[::1] runs = np.zeros(Y.shape[0], dtype=np.int64)
-    cdef long[:] grp_runs
-    cdef double[::1] Ycoord = np.zeros(Y.shape[1], dtype=np.double)
-    cdef double[::1] co = np.zeros(Y.shape[1], dtype=np.double)
-    cdef double[:, :] Xrows = np.zeros((X.shape[0], X.shape[1]), dtype=np.double)
-    cdef double[:, :] new_X_rows
-    cdef double[:, :] possible_coords_arr
+    cdef long[::1] grp_view
+    cdef long[::1] runs = np.zeros(Y_view.shape[0], dtype=np.int64)
+    cdef double[::1] Ycoord = np.zeros(Y_view.shape[1], dtype=np.double)
+    cdef double[::1] co = np.zeros(Y_view.shape[1], dtype=np.double)
+    cdef double[:,::1] Xrows = np.zeros((X_view.shape[0], X_view.shape[1]), dtype=np.double)
+    cdef double[:,::1] new_X_rows
+    cdef double[:,::1] possible_coords_arr
+    cdef double[::1] new_coord
+    cdef cnp.ndarray[cnp.double_t, ndim=2, mode='c'] new_Y_rows
+
+    ###############################################
 
     # Make sure we are not stuck in infinite loop
     for it in range(max_it):
@@ -65,39 +84,47 @@ cpdef _optimize_cython_impl(object params, int max_it, bint validate, double eps
             level = effect_levels[i]
             possible_coords_arr = coords[i]
             slice_len = colstart[i+1] - colstart[i]
+            grp_view = grps[i]
 
             # Loop over all run-groups for this factor
-            for grp_val in grps[i]:
+            for j in range(grp_view.shape[0]):
+                grp_val = grp_view[j]
+
                 # Determine runs affected by this group: TODO: precompute
                 if level == 0:
                     nruns = 1
                     runs[0] = grp_val
                 else:
-                    grp_runs = np.flatnonzero(Zs[level-1] == grp_val)
-                    nruns = grp_runs.shape[0]
-                    runs[:nruns] = grp_runs
+                    nruns = 0
+                    for k in range(Zs.shape[1]):
+                        if Zs[level-1][k] == grp_val:
+                            runs[nruns] = k
+                            nruns += 1
 
-                # Take from the first run in the group
+                # Store the coordinate and affected rows
                 Ycoord[:slice_len] = Y_view[runs[0], colstart[i]:colstart[i+1]]
                 co[:slice_len] = Ycoord[:slice_len]
                 for run_idx in range(nruns):
-                    Xrows[runs[run_idx]] = X_view[runs[run_idx]]
-
+                    Xrows[run_idx] = X_view[runs[run_idx]]
+                
                 # Loop over possible new coordinates
-                for new_coord in possible_coords_arr:
+                for h in range(possible_coords_arr.shape[0]):
+                    new_coord = possible_coords_arr[h]
 
                     # Short-circuit original coordinates
-                    if not np.array_equal(new_coord, co):
-                        
+                    if not cython_equal(new_coord, co, slice_len):
                         # Update Y_view
                         for run_idx in range(nruns):
                             Y_view[runs[run_idx], colstart[i]:colstart[i+1]] = new_coord
 
+                        # Get the affected runs
+                        new_Y_rows = Y[runs[:nruns], :]
+
                         # Validate whether to check the coordinate
-                        if not np.any(fn.constraints(Y[runs[:nruns]])):
+                        if not cython_any(fn.constraints(new_Y_rows), nruns):
                             
                             # Update X_view
-                            new_X_rows = fn.Y2X(Y[runs[:nruns]])
+                            new_X_rows = fn.Y2X(new_Y_rows)
                             for run_idx in range(nruns):
                                 X_view[runs[run_idx]] = new_X_rows[run_idx]
 
@@ -106,11 +133,11 @@ cpdef _optimize_cython_impl(object params, int max_it, bint validate, double eps
                             up = new_metric - current_metric
 
                             # New best design
-                            if ((current_metric == 0 or np.isinf(current_metric)) and up > 0) or up / np.abs(current_metric) > eps:
+                            if ((current_metric == 0 or isinf(current_metric)) and up > 0) or up / abs(current_metric) > eps:
                                 # Store the best coordinates found so far
                                 Ycoord[:slice_len] = new_coord
                                 for run_idx in range(nruns):
-                                    Xrows[runs[run_idx]] = X_view[runs[run_idx]]
+                                    Xrows[run_idx] = X_view[runs[run_idx]]
                                 current_metric = new_metric
                                 updated = True
 
