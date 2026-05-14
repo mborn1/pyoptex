Bugfix in obs_var_from_Zs and woodbury formula for non-sequential Zi

src/pyoptex/doe/cost_optimal/cov.py
@@ -38,7 +38,7 @@ def _update_woodbury(Vinv, new_Zs, new_ratios):
     assert len(new_Zs) == new_ratios.shape[1], "Every grouping requires a ratio"
 
     # Convert to expanded format
-    new_Zs = [np.eye(Zi[-1] + 1, dtype=np.bool_)[Zi] for Zi in new_Zs]
+    new_Zs = [np.eye(np.max(Zi) + 1, dtype=np.bool_)[Zi] for Zi in new_Zs]
 
     # Apply each Zs sequentially
     for i in range(len(new_Zs)):

src/pyoptex/utils/design.py
@@ -62,7 +62,9 @@ def obs_var_from_Zs(Zs, N, ratios=None, include_error=True):
 
     return V + sum(
         r * (Zi_expanded @ Zi_expanded.T)
-        for r, Zi_expanded in ((r, np.eye(Zi[-1] + 1)[Zi]) for r, Zi in zip(ratios, Zs, strict=True) if Zi is not None)
+        for r, Zi_expanded in (
+            (r, np.eye(np.max(Zi) + 1)[Zi]) for r, Zi in zip(ratios, Zs, strict=True) if Zi is not None
+        )
     )
 
 